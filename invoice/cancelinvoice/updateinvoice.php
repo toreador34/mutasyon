8228daced08f81aede7da0980f1a31f0929d7711
worlds <vbenek@yahoo.com>
2016-03-22 21:44:52 +0200

    Delete updateinvoice.php

--- invoice/cancelinvoice/updateinvoice.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-	  $updateinv = $db->prepare("UPDATE invoice SET invoice_description = :desc, invoice_cancelled =  :canc WHERE  invoice.invoice_id =:id");
-	    $updateinv->execute(array(
-				    'desc'=>$bdesc."-".$due,
-				    'canc'=>1,
-				    'id'=>$id,
-			    ));
-	     
